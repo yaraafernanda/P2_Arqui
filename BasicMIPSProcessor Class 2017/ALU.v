9849d0a784b1ec3ad3da127ecbdedf7372d12352
Miguel Angel <tpo.m.ang@gmail.com>
2018-04-16 13:04:34 -0500

    J JAL JR implemented

--- BasicMIPSProcessor Class 2017/ALU.v
+++ BasicMIPSProcessor Class 2017/ALU.v
@@ -23,6 +23,7 @@ module ALU
 	input [31:0] A,
 	input [31:0] B,
 	output reg Zero,
+	output reg Jr,
 	output reg [31:0]ALUResult
 );
 
@@ -35,6 +36,7 @@ localparam NOR = 4'b0101;
 localparam OR	= 4'b0001;
 localparam SLL = 4'b0100;
 localparam SRL = 4'b0110;
+localparam JR 	= 4'b1000;
    
    always @ (A or B or ALUOperation)
      begin
@@ -55,10 +57,14 @@ localparam SRL = 4'b0110;
 		   ALUResult = A << B;
 		  SRL: //Shift left
 		   ALUResult = A >> B;
+		  JR:	//Jump Register
+			ALUResult = A;
 			
 		default:
 			ALUResult= 0;
 		endcase // case(control)
 		Zero = (ALUResult==0) ? 1'b1 : 1'b0;
+		Jr = (ALUOperation==JR)? 1'b1: 1'b0;
+		
      end // always @ (A or B or control)
 endmodule // ALU
